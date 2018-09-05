2d3af8bd5b08adc82bcdc43bd85aa3d8866b39a8
Jorge <jarriolapo@gmail.com>
2018-09-05 12:48:19 +0200

    vamos mejorando Con NODE

--- db.js
+++ db.js
@@ -4,17 +4,33 @@ const mysql = require('mysql')
 //CREAMOS UNA VARIABLE Y LA INICIALIZAMOS EN NULL
 let pool = null
 
-//CON EXPORT PUEDO ACCDER A ESTAS FUNCIONES DESDE AFUERA
+//CON EXPORT PUEDO ACCDER A ESTAS FUNCIONES DESDE AFUERA 
+//ESTA ES MI BBDD LOCAL
+        // exports.connect = (done) => {
+        //     pool = mysql.createPool({
+        //     host: 'localhost',
+        //     user: 'root',
+        //     password: 'root',
+        //     database: 'toknow',
+        //     port:8889
+        //     })
+        //     done()
+        // }
+
+//ESTA ES MI BBDD PROVICIONAL EN HEROKU MARIO
 exports.connect = (done) => {
     pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'toknow',
-    port:8889
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'bc98861819afa8',
+    password: '37b4811a',
+    database: 'heroku_b5b45e98e7b2de8',
+    port:3306
     })
     done()
 }
+
+
+
 exports.get = () => {
  return pool   
 }
